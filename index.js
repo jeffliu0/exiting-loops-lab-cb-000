bd40905479abacc6baa11f36e741bdaeabd016d2
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-20 06:29:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   var bool = null
   for(var i = 0; i < array.length; i++){
-    if(findFn == true){
+    if(findFn.arguments(0) == true){
       bool = array[i]
     }
   }
